f3b8d50b6bd55a3f385e88b6bc5f42f93d929838
YuriASN <yuriasn@hotmail.com>
2023-05-15 00:20:25 +0100

    Added msg handler and time functions

--- utils.c
+++ utils.c
@@ -1,9 +1,9 @@
 #include "philo.h"
 
 /*The memset() function writes len bytes of value c (converted to an
-unsigned char) to the string dest.	 Undefined behaviour from memset(),
+unsigned char) to the string dest. Undefined behaviour from memset(),
 resulting from storage overflow, will occur if len	is greater than	the
-length of the dest	buffer.	 The behaviour is also undefined if dest is an
+length of the dest	buffer. The behaviour is also undefined if dest is an
 invalid pointer. */
 static void	*ft_memset(void *str, int c, int bytes)
 {
@@ -30,4 +30,36 @@ void	*ft_calloc(size_t number, size_t size)
 		return (0);
 	ft_memset(a, 0, (number * size));
 	return (a);
+}
+
+/* Returns current time in microseconds. */
+size_t	time_now(t_data *data)
+{
+	ssize_t			now;
+	struct timeval	time;
+
+	pthread_mutex_lock(&data->time);
+	gettimeofday(&time, NULL);
+//printf("boot time = %ld\t", data->boot_time);
+	now = ((time.tv_sec * 1000) + (time.tv_usec / 1000)) - data->boot_time;
+	pthread_mutex_unlock(&data->time);
+//printf("%snow = %ld%s\n", GRN, now, CRESET);
+	return ((size_t)now);
+}
+
+/* Sends the message using mutex according to msg number. */
+void	msg_go(t_data *data, int msg, ssize_t time, ssize_t philo)
+{
+	pthread_mutex_lock(&data->msg);
+	if (msg == DIED)
+		printf("%ldms %ld died\n", time, philo);
+	else if (msg == EAT)
+		printf("%ldms %ld is eating\n", time, philo);
+	else if (msg == SLEEP)
+		printf("%ldms %ld is sleeping\n", time, philo);
+	else if (msg == THINK)
+		printf("%ldms %ld is thinking\n", time, philo);
+	else if (msg == FORK)
+		printf("%ldms %ld has taken a fork\n", time, philo);
+	pthread_mutex_unlock(&data->msg);
 }
